ab357a1b0431876863a72f51e1418e7895d429db
Jyotirmoy Das <jyotimoydas12345@gmail.com>
2026-02-02 01:10:20 +0530

    Prepare release v2.2.2, Major Change to Chart Marker- now users can change the Chatmarker by clicking on the Specific Subject's Chart even after creation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "in.jyotirmoy.attendx"
         minSdk = 26
         targetSdk = 36
-        versionCode = 17
-        versionName = "2.2.0"
+        versionCode = 18
+        versionName = "2.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
--- app/src/main/java/in/jyotirmoy/attendx/settings/data/local/source/VersionList.kt
+++ app/src/main/java/in/jyotirmoy/attendx/settings/data/local/source/VersionList.kt
@@ -1,6 +1,7 @@
 package `in`.jyotirmoy.attendx.settings.data.local.source
 
 val versionList = listOf(
+    "v2.2.2",
     "v2.2.0",
     "v2.0.0",
     "v1.9.0",
